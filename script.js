e1587f47439f4d37a285465a8abf2d42e4e81ffc
Alozoka <141640569+Alozoka@users.noreply.github.com>
2025-03-13 11:37:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+    const alternaContraste = document.getElementById('alterna-contraste');
 
     let tamanhoAtualFonte = 1;
 
@@ -24,4 +25,8 @@ document.addEventListener('DOMContentLoaded', function () {
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
     })
+
+    alternaContraste.addEventListener('click', function(){ 
+        document.body.classList.toogle('alto-contraste')
+    }
 })
